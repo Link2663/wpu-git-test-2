eb8cfc5df4ba9bdb9c6c71889780e2b5bddff237
Link2663 <jessicaforlast@gmail.com>
2021-08-09 12:35:28 +0800

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
 	<h1>Hello World!</h1>
 	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio ut corporis id fugiat, minus voluptates praesentium rerum rem culpa consequatur similique explicabo. Doloremque quia quaerat qui suscipit dignissimos corrupti sit.</p>
+	<ul>
+		<li>satu</li>
+		<li>dua</li>
+	</ul>
 </body>
 </html>
